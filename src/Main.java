d2787cf31fb5b508de28f57d8f83cb4fdf507ba1
Joel2775 <joel.acosta@epn.edu.ec>
2025-12-04 13:28:01 -0500

    se agrega el try- catch

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== CUENTA DE AHORROS ===");
-        CuentaAhorros ca = new CuentaAhorros("1234567890", "Juan Perez", 200);
-        ca.depositar(300);
-        ca.retirar(450);
+        try {
+            System.out.println("=== CUENTA DE AHORROS ===");
+            CuentaAhorros ca = new CuentaAhorros("1234567890", "Juan Perez", 200);
+            ca.depositar(300);
+            ca.retirar(450);
 
-        System.out.println("\n=== CUENTA CORRIENTE ===");
-        CuentaCorriente cc = new CuentaCorriente("0987654321", "Maria Nuñes", 1000);
-        cc.retirar(1200);
-        cc.trasnferir(300,"0147258369");
+            System.out.println("\n=== CUENTA CORRIENTE ===");
+            CuentaCorriente cc = new CuentaCorriente("0987654321", "Maria Nuñes", 1000);
+            cc.retirar(1200);
+            cc.trasnferir(300,"0147258369");
 
-        System.out.println("\n=== SALDOS FINALES ===");
-        System.out.println("Cuenta Ahooros: " + ca.consultarSaldo());
-        System.out.println("Cuenta Corriente: " + cc.consultarSaldo());
+            System.out.println("\n=== SALDOS FINALES ===");
+            System.out.println("Cuenta Ahooros: " + ca.consultarSaldo());
+            System.out.println("Cuenta Corriente: " + cc.consultarSaldo());
+        }catch (IllegalArgumentException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
